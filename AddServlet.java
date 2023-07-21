9afdae7a12e30b0eda966053b0e2e4e8167aa14f
NehaRaut4 <sneharaut8792@gmail.com>
2023-07-21 20:54:28 +0530

    method as get at client side and post (doPost) at Server side

--- AddServlet.java
+++ AddServlet.java
@@ -10,7 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 	//service method to write business logic it has two parameters request and response
 	//as it uses http protocol so both request and response are of HttpServlet type
-	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
+	//doPost() methods accept request through post method from client
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
 	{
 		//accepting i/p parameters from client i.e num1 and num2
 		int n1 = Integer.parseInt(req.getParameter("num1"));
